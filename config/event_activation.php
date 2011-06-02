74596de685548f1f7a3066fe3685b4bd5299312e
trulycool <morgan@trulycool.com>
2011-06-02 17:22:42 -0400

    Updated table creation logic; based on existence of table, not config setting.

--- config/event_activation.php
+++ config/event_activation.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Event Activation
  *
@@ -11,94 +12,100 @@
  */
 class EventActivation {
 
-	public $version = '1.0.1';
+    public $version = '1.0.2';
 
-/**
- * onActivate will be called if this returns true
- *
- * @param  object $controller Controller
- * @return boolean
- */
+    /**
+     * onActivate will be called if this returns true
+     *
+     * @param  object $controller Controller
+     * @return boolean
+     */
     public function beforeActivation(&$controller) {
-        $current_version  = Configure::read('Event.version');
-        if($this->version != $current_version){
-                switch($this->version){
-                        case '1.0.1':
-                        default:
-                        // Add a table to the DB
-                        App::import('Core', 'File');
-                        App::import('Model', 'CakeSchema', false);
-                                App::import('Model', 'ConnectionManager');
+        $current_version = Configure::read('Event.version');
+//        if ($this->version != $current_version) {//ORIG
+        if (!$this->check_table_exists('events')) {
+            // create the table if it doesn't already exist
+            switch ($this->version) {
+                case '1.0.2':
+                default:
+                    // Add a table to the DB
+                    App::import('Core', 'File');
+                    App::import('Model', 'CakeSchema', false);
+                    App::import('Model', 'ConnectionManager');
 
-                                $db = ConnectionManager::getDataSource('default');
-                                if(!$db->isConnected()) {
-                                                $this->Session->setFlash(__('Could not connect to database.', true));
-                                        } else {
-                                                $schema =& new CakeSchema(array('plugin'=>'event','name'=>'event'));
-                                                $schema = $schema->load();
-                                                foreach($schema->tables as $table => $fields) {
-                                                        $create = $db->createSchema($schema, $table);
-                                                        $db->execute($create);
-                                                } 
-                                }      
+                    $db = ConnectionManager::getDataSource('default');
+                    if (!$db->isConnected()) {
+                        $this->Session->setFlash(__('Could not connect to database.', true));
+                    } else {
+                        $schema = & new CakeSchema(array('plugin' => 'event', 'name' => 'event'));
+                        $schema = $schema->load();
+                        foreach ($schema->tables as $table => $fields) {
+                            $create = $db->createSchema($schema, $table);
+                            $db->execute($create);
+                        }
+                    }
 
-                        // Main menu: add an Example link
-                        $mainMenu = $controller->Link->Menu->findByAlias('main');
-                        $controller->Link->Behaviors->attach('Tree', array(
-                            'scope' => array(
-                                'Link.menu_id' => $mainMenu['Menu']['id'],
-                            ),
-                        ));
-                        $controller->Link->save(array(
-                            'menu_id' => $mainMenu['Menu']['id'],
-                            'title' => 'Events',
-                            'link' => 'plugin:event/controller:event/action:index',
-                            'status' => 1,
-                        ));
-                        break;
-        }
-                $controller->Setting->write('Event.version', $this->version, array('editable' => 0, 'title' => 'Version'));
+                    // Main menu: add an Example link
+                    $mainMenu = $controller->Link->Menu->findByAlias('main');
+                    $controller->Link->Behaviors->attach('Tree', array(
+                        'scope' => array(
+                            'Link.menu_id' => $mainMenu['Menu']['id'],
+                        ),
+                    ));
+                    $controller->Link->save(array(
+                        'menu_id' => $mainMenu['Menu']['id'],
+                        'title' => 'Events',
+                        'link' => 'plugin:event/controller:event/action:index',
+                        'status' => 1,
+                    ));
+                    break;
+            }
+            $controller->Setting->write('Event.version', $this->version, array('editable' => 0, 'title' => 'Version'));
         }
         return true;
-    }/**
- * Called after activating the hook in ExtensionsHooksController::admin_toggle()
- *
- * @param object $controller Controller
- * @return void
- */
+    }
+
+/**
+     * Called after activating the hook in ExtensionsHooksController::admin_toggle()
+     *
+     * @param object $controller Controller
+     * @return void
+     */
+
     public function onActivation(&$controller) {
         // ACL: set ACOs with permissions
         $controller->Croogo->addAco('Event'); // ExampleController
         $controller->Croogo->addAco('Event/admin_index'); // ExampleController::admin_index()
         $controller->Croogo->addAco('Event/index', array('registered', 'public')); // ExampleController::index()
-        $controller->Croogo->addAco('Event/calendar', array('registered', 'public')); // ExampleController::index()        
-     }
-/**
- * onDeactivate will be called if this returns true
- *
- * @param  object $controller Controller
- * @return boolean
- */
+        $controller->Croogo->addAco('Event/calendar', array('registered', 'public')); // ExampleController::index()
+    }
+
+    /**
+     * onDeactivate will be called if this returns true
+     *
+     * @param  object $controller Controller
+     * @return boolean
+     */
     public function beforeDeactivation(&$controller) {
         return true;
     }
-/**
- * Called after deactivating the plugin in ExtensionsPluginsController::admin_toggle()
- *
- * @param object $controller Controller
- * @return void
- */
+
+    /**
+     * Called after deactivating the plugin in ExtensionsPluginsController::admin_toggle()
+     *
+     * @param object $controller Controller
+     * @return void
+     */
     public function onDeactivation(&$controller) {
         // ACL: remove ACOs with permissions
         $controller->Croogo->removeAco('Event'); // ExampleController ACO and it's actions will be removed
-
         // Main menu: delete Event link
         $link = $controller->Link->find('first', array(
-            'conditions' => array(
-                'Menu.alias' => 'main',
-                'Link.link' => 'plugin:event/controller:event/action:index',
-            ),
-        ));
+                    'conditions' => array(
+                        'Menu.alias' => 'main',
+                        'Link.link' => 'plugin:event/controller:event/action:index',
+                    ),
+                ));
         $controller->Link->Behaviors->attach('Tree', array(
             'scope' => array(
                 'Link.menu_id' => $link['Link']['menu_id'],
@@ -107,8 +114,17 @@ class EventActivation {
         if (isset($link['Link']['id'])) {
             $controller->Link->delete($link['Link']['id']);
         }
+    }
 
+    function check_table_exists($table) {
+        //returns 1 if table exists, no return otherwise
+        App::import('Core', 'File');
+        App::import('Model', 'ConnectionManager');
+        $dbo = ConnectionManager::getDataSource('default');
+        $result = $dbo->_execute('DESCRIBE ' . $table);
+        return!empty($result);
     }
 
 }
+
 ?>
